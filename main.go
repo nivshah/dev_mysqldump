f303c358eea1ec58e455846a88df71212740964b
Niv Shah <nivshah@gmail.com>
2021-02-03 11:44:52 -0500

    Add SSL cert support and fixes port support

--- main.go
+++ main.go
@@ -8,8 +8,10 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"crypto/x509"
+	"crypto/tls"
     "gopkg.in/yaml.v2"
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-sql-driver/mysql"
 )
 
 func handleError(err interface{}) {
@@ -18,7 +20,7 @@ func handleError(err interface{}) {
 	}
 }
 
-func analyze(user, host, port, password, database string, db *sql.DB) {
+func analyze(user, host, port, password, database, ssl_ca string, db *sql.DB) {
 	// To get biggest tables
 	query := "SELECT table_name, coalesce(round(((data_length + index_length) / 1024 / 1024), 2), 0.00)"
 	query += "FROM information_schema.TABLES WHERE table_schema = ? ORDER BY (data_length + index_length) DESC"
@@ -37,6 +39,11 @@ func analyze(user, host, port, password, database string, db *sql.DB) {
 		if table_size > big_table_size {
 			// Could this be done from existing connection?
 			mysql_cmd := "mysql --user " + user + " --host " + host + " --port " + port + " -p" + password + " "
+
+			if ssl_ca != "" {
+				mysql_cmd += "--ssl-ca " + ssl_ca + " "
+			}
+
 			mysql_cmd += "--table --execute 'DESCRIBE " + table_name + ";' " + database
 
 			out, err := exec.Command("/bin/bash", "-c", mysql_cmd).Output()
@@ -54,7 +61,7 @@ func analyze(user, host, port, password, database string, db *sql.DB) {
 
 }
 
-func dump(user, host, port, password, database, config_file string, db *sql.DB) {
+func dump(user, host, port, password, database, config_file, ssl_ca string, db *sql.DB) {
 	type DbDumpConfig struct {
 		Tables []struct {
 			TableName string `yaml:"table_name"`
@@ -115,7 +122,13 @@ func dump(user, host, port, password, database, config_file string, db *sql.DB) 
 		table := db_tables[i]
 		log.Println("Running mysql_dump for", table.table_name)
 		command := "mysqldump --lock-tables=false --compact "
-		command += "--host " + host + " --user " + user + " -p" + password + " "
+		command += "--host " + host + " --port " + port + " "
+		command += "--user " + user + " -p" + password + " "
+
+		if ssl_ca != "" {
+			command += "--ssl-ca " + ssl_ca + " "
+		}
+
 		command += "--where=\"" + table.where + "\" "
 		command += table.flags + " "
 		command += database + " " + table.table_name
@@ -135,10 +148,19 @@ func dump(user, host, port, password, database, config_file string, db *sql.DB) 
 	}
 
 	// Dump the views too
-	command := "mysql --host " + host + " --user " + user + " -p" + password + " "
+	command := "mysql --host " + host + " --port " + port + " --user " + user + " -p" + password + " "
+
+	if ssl_ca != "" {
+		command += "--ssl-ca " + ssl_ca + " "
+	}
+
 	command += "INFORMATION_SCHEMA  --skip-column-names --batch "
 	command += "-e \"select table_name from tables where table_type = 'VIEW' and table_schema = '" + database + "'\""
-	command += "| xargs mysqldump --host " + host + " --user " + user + " -p" + password + " " + database + " "
+	command += "| xargs mysqldump --host " + host + " --port " + port + " --user " + user + " -p" + password + " " + database + " "
+
+	if ssl_ca != "" {
+		command += "--ssl-ca " + ssl_ca + " "
+	}
 	// And get rid of the DEFINER statements on the views, because they end up causing 'access denied' issues
 	command += "| sed -e 's/DEFINER[ ]*=[ ]*[^*]*\\*/\\*/'"
 	log.Println("Cmd: ", command)
@@ -160,14 +182,37 @@ func dump(user, host, port, password, database, config_file string, db *sql.DB) 
 func main() {
 	var user = flag.String("user", "root", "The mysql user")
 	var host = flag.String("host", "localhost", "The mysql host")
-	var port = flag.String("post", "3306", "The mysql post")
+	var port = flag.String("port", "3306", "The mysql port")
 	var password = flag.String("password", "", "the password for this user")
 	var database = flag.String("database", "", "The database name")
+	var ssl_ca = flag.String("ssl-ca", "", "Path to SSL cert (if used)")
 	var mode = flag.String("mode", "analyze", "Valid options are 'analyze' or 'dump")
 	var config_file = flag.String("config", "", "The yaml config for 'dump' mode")
 	flag.Parse()
 
-	db, err := sql.Open("mysql", *user+":"+*password+"@("+*host+":"+*port+")/"+*database)
+	tlsString := "false"
+
+	if *ssl_ca != "" {
+		// we have a cert
+		rootCertPool := x509.NewCertPool()
+		pem, err := ioutil.ReadFile(*ssl_ca)
+		if err != nil {
+	       log.Fatal(err)
+	    }
+
+		if ok := rootCertPool.AppendCertsFromPEM(pem); !ok {
+			log.Fatal("Failed to append PEM.")
+	    }
+
+		mysql.RegisterTLSConfig("custom", &tls.Config{
+			ServerName: *host,
+			RootCAs: rootCertPool,
+		})
+
+		tlsString = "custom"
+	}
+
+	db, err := sql.Open("mysql", *user+":"+*password+"@("+*host+":"+*port+")/"+*database+"?tls="+tlsString)
 	handleError(err)
 	defer db.Close()
 
@@ -176,9 +221,9 @@ func main() {
 
 	switch *mode {
 	case "analyze":
-		analyze(*user, *host, *port, *password, *database, db)
+		analyze(*user, *host, *port, *password, *database, *ssl_ca, db)
 	case "dump":
-		dump(*user, *host, *port, *password, *database, *config_file, db)
+		dump(*user, *host, *port, *password, *database, *ssl_ca, *config_file, db)
 	default:
 		log.Fatal("No valid mode provided! Please seee --help")
 	}
